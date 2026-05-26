95057cb7373f6d33ba256a3a36d11dbd4d9f0be6
FancyCherepahe <elektrodanulo@gmail.com>
2026-05-26 13:23:07 +0300

    chaned the discord chaneel name

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let discordLinksDisplayState = false;
 function showDiscordLinks() {
     const discordLinks = [
         
-        {link: "https://discord.gg/WFJnEfFAtt", name: 'Murder Drones "Official"', img: "images/logo.png"},
+        {link: "https://discord.gg/WFJnEfFAtt", name: 'JCJenson Space Corps', img: "images/logo.png"},
         
         {link: "https://discord.gg/67W39DqJ9G", name: "Aure`s Basement", img: "images/logo.png"},
     
